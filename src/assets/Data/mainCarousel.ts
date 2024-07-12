4cf831d6eefe85e6171d3fe021afe707210f95e0
amargaddam10 <amargaddam1212@gmail.com>
2024-07-12 12:16:14 +0530

    Initial commit

---
+++ src/assets/Data/mainCarousel.ts
@@ -0,0 +1,22 @@
+export const homeCarouselData = [
+    {
+        image:"https://m.media-amazon.com/images/G/31/img24/MA/July/casualwear/af_top_pc._CB554486608_.jpg"
+        
+    },
+    {
+        image:"https://m.media-amazon.com/images/G/31/img23/WA/2024/may/ss-flip/kurta/hero/Kurtas-sets-more_1._CB558376925_.png"
+        
+    },
+    {
+        image:"https://m.media-amazon.com/images/G/31/img24/Fashion/AF/UBSJuly24/TopHero/PC/Footwear_1500x400._CB569892377_.png"
+        
+    },
+    {
+        image:"https://m.media-amazon.com/images/G/31/img24/Fashion/AF/UBSJuly24/TopHero/PC/Elevated_Classis_1500x400._CB569892377_.png"
+        
+    },
+    {
+        image:"https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/Sareestore/PC/Fabric/4._CB599785833_.jpg"
+        
+    },
+]
